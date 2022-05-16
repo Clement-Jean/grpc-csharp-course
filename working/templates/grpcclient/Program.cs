4ce8227c69c5d061980a9a9a59da6700c5877026
Clement <clement.jean@epitech.eu>
2022-05-16 15:23:27 +0800

    remove working dir

--- working/templates/grpcclient/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿using Grpc.Net.Client;
-using Grpc.Core;
-
-using var channel = GrpcChannel.ForAddress("http://localhost:50051");
--- working/templates/grpcserver/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddGrpc();
-var app = builder.Build();
-
-app.Run();
